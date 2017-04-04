d83060e28f667509414fc64af06dba2a35d0badf
Charlie Anderson <charlieanderson13@gmail.com>
2017-04-04 12:09:15 -0700

    some ui tweaks to the buttons

--- public/javascripts/pomodoro.js
+++ public/javascripts/pomodoro.js
@@ -1,5 +1,6 @@
 var interval = null;
 var workMode = true;
+var paused = false;
 var tau = 2*Math.PI;
 var secs = 0,
     mins = 0.0,
@@ -8,7 +9,6 @@ var secs = 0,
     rest = 0.0;
 
 function start() {
-    var task = (String)(document.getElementById("input-task").value);
     var toggleChecked = document.getElementById("toggle-off").checked;
     workMode = toggleChecked;
     work = (document.getElementById("input-work").value)/1;
@@ -18,7 +18,7 @@ function start() {
         mins = work;
     else
         mins = rest;
-    createClock(task, work, rest);
+    createClock(work, rest);
 }
 
 function stop() {
@@ -35,28 +35,46 @@ function stop() {
     mins = 0;
     minsElapsed = 0;
     workMode = toggleChecked;
+    paused = false;
 }
 
-function createClock(task, work, rest) {
+function createClock(work, rest) {
     // setup the arcs for the clock
     var arc = d3.arc();
     var timeText = "00:00";
     var arcSeconds = d3.arc()
-        .innerRadius(170)
-        .outerRadius(220)
+        .innerRadius(150)
+        .outerRadius(200)
         .startAngle(0);
     var arcMinutes = d3.arc()
-        .innerRadius(115)
-        .outerRadius(165)
+        .innerRadius(95)
+        .outerRadius(145)
         .startAngle(0);
+
+    addPauseButton();
+
     // setup svg, g is set so that the origin is the center of the canvas
     var svg = d3.select("svg"),
         width = +svg.attr("width"),
         height = +svg.attr("height"),
+        gButton = svg.append("g")
+                .attr("onclick", "pause()")
+                .attr("transform", "translate("+width/2+","+(height)+")");
         gSeconds = svg.append("g")
                 .attr("transform", "translate("+width/2+","+height/2+")");
         gMinutes = svg.append("g")
                 .attr("transform", "translate("+width/2+","+height/2+")");
+
+    // create pause button on svg
+    var pauseButton = gButton.append("text")
+        .text("Pause")
+        .style("fill", "#eee")
+        .attr("class", "start-button")
+        .attr("text-anchor", "middle")
+        .attr("font-size", "24px")
+        .attr("float","right")
+        .attr("onclick", "function() { pause(); };")
+        .attr("font-family", "helvetica");
     // setup the background arc, which will be filled by the main arc
     var backgroundSeconds = gSeconds.append("path")
         .datum({endAngle: tau})
@@ -86,41 +104,47 @@ function createClock(task, work, rest) {
         .attr("font-size", "48px")
         .attr("font-family", "helvetica");
 
+    pauseButton.onclick = function() { pause(); };
     interval = d3.interval(function() {
-        secs--;
-        console.log(secs);
-        if(secs < 0) {
-            secs = 59;
-            minsElapsed++;
-        }
-        console.log(minsElapsed/mins);
-        timeText = getTimeText();
-        clockText.text(timeText);
-        foregroundSeconds.transition()
-            .duration(1000)
-            .attrTween("d", arcTween((60-secs)/(60)*tau, arcSeconds));
-        foregroundMinutes.transition()
-            .duration(1000)
-            .attrTween("d", arcTween((minsElapsed-1)/(mins)*tau, arcMinutes));
-        if(workMode) {
-            foregroundSeconds.style("fill", "#FF0043")
-            foregroundMinutes.style("fill", "#C00043")
+        if(paused) {
+
         }
         else {
-            foregroundSeconds.style("fill", "#34F898")
-            foregroundMinutes.style("fill", "#00E174")
-        }
-        if(workMode && minsElapsed === work && secs === 0) {
-            mins = rest;
-            secs = 0;
-            minsElapsed = 0;
-            workMode = false;
-        }
-        else if (!workMode && minsElapsed === rest && secs === 0) {
-            mins = work;
-            secs = 0;
-            minsElapsed = 0;
-            workMode = true;
+            secs--;
+            console.log(secs);
+            if(secs < 0) {
+                secs = 59;
+                minsElapsed++;
+            }
+            console.log(minsElapsed/mins);
+            timeText = getTimeText();
+            clockText.text(timeText);
+            foregroundSeconds.transition()
+                .duration(1000)
+                .attrTween("d", arcTween((60-secs)/(60)*tau, arcSeconds));
+            foregroundMinutes.transition()
+                .duration(1000)
+                .attrTween("d", arcTween((minsElapsed-1)/(mins)*tau, arcMinutes));
+            if(workMode) {
+                foregroundSeconds.style("fill", "#FF0043")
+                foregroundMinutes.style("fill", "#C00043")
+            }
+            else {
+                foregroundSeconds.style("fill", "#34F898")
+                foregroundMinutes.style("fill", "#00E174")
+            }
+            if(workMode && minsElapsed === work && secs === 0) {
+                mins = rest;
+                secs = 0;
+                minsElapsed = 0;
+                workMode = false;
+            }
+            else if (!workMode && minsElapsed === rest && secs === 0) {
+                mins = work;
+                secs = 0;
+                minsElapsed = 0;
+                workMode = true;
+            }
         } 
     }, 1000);
 
@@ -144,6 +168,28 @@ function createClock(task, work, rest) {
             minsText = "0"+minsText;
         }
         return minsText+":"+secsText;
+    }
+
+    function addPauseButton(){
+        var actions_div = document.getElementById('actions-div');
+        var pause_button = document.createElement('button');
+        pause_button.setAttribute('id', 'pause-button');
+        pause_button.setAttribute("class", "start-button");
+        pause_button.innerHTML = "Pause";
+        pause_button.onclick = function() { pause(); };
+        actions_div.appendChild(pause_button);
+    }
+
+    function pause(){
+        paused = !paused;
+        var pause_button = document.getElementById("pause-button");
+        if(paused === false) {
+            pause_button.innerHTML = "Pause";
+        }
+        else {
+            pause_button.innerHTML = "Resume";
+        }
+        console.log("Pausing...");
     }
 }
 
